c72b3f4e1963307ab5c45e165eb5c55ab9949c38
Abinash Mohanty <abinashmohanty@Abinashs-MacBook-Air.local>
2026-05-21 18:06:03 +0530

    Save all changes

---
+++ models.py
@@ -0,0 +1,4 @@
+# contains all my database models. I am using Flask-SQLAlchemy ORM to interact with a SQLite database programmatically.
+from flask_sqlalchemy import SQLAlchemy  # type: ignore
+
+db = SQLAlchemy()
